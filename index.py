043726d8190b75e744f5ba205fe9e479425b01a0
hanniferjebe <hanniferjebet@gmail.com>
2025-03-23 18:21:40 +0300

    week one assignment

---
+++ index.py
@@ -0,0 +1,21 @@
+def calculate(num1, num2, operation):
+    if operation == "+":    
+        return num1 + num2
+    elif operation == "-": 
+        return num1 - num2
+    elif operation == "*":  
+        return num1 * num2
+    elif operation == "/":
+        return num1 / num2
+    else:
+        return "Invalid operation"
+    return
+
+num1 = float(input("enter the first number: "))
+
+operation = input("enter the operation(+, -, *, /): ")
+
+num2 - float(input("enter the second number: "))
+result = calculate(num1, num2, operation)
+
+print(f"result: {result}")
